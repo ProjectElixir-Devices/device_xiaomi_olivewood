b5a9e19e15348fa4fdce603f2fdb30505edfae40
JaswalAshish <jaswalashish48@gmail.com>
2022-04-15 20:55:51 +0530

    olivewood: Add vendorsetup.sh
    Signed-off-by: JaswalAshish <jaswalashish48@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+rm -rf packages/resources/devicesettings
+git clone https://github.com/PixelExperience/packages_resources_devicesettings.git packages/resources/devicesettings
+git clone --depth=1 https://github.com/ProjectElixir-Devices/vendor_xiaomi_olivewood.git vendor/xiaomi/olivewood
+git clone --depth=1 https://github.com/ProjectElixir-Devices/kernel_xiaomi_olivewood.git kernel/xiaomi/olivewood
